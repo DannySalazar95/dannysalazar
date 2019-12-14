517fc19f51b9327192c9a761b333a89065143514
Danny Salazar <danny.namuche.1995@gmail.com>
2019-12-14 04:34:09 -0500

    solution url's experience component

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -5,7 +5,7 @@
             <div v-for="({company, logo,description,link,work_dates},index) in works" :key="index" class="w-full border border-gray-400 bg-white p-4 flex flex-col justify-between leading-normal mb-1">
                 <div class="mb-8">
                     <p class="text-sm text-gray-600 text-right">{{company}}</p>
-                    <img class="mb-2" :src="logo.url" :alt="company" :width="logo.width" :height="logo.height">
+                    <img class="mb-2" :src="`${url}${logo.url}`" :alt="company" :width="logo.width" :height="logo.height">
                     <hr>
                     <p class="flex text-gray-800 text-base mt-2">
                         <svg class="fill-current text-gray-800 w-4 h-5 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M148 288h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm108-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 96v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96-260v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48zm-48 346V160H48v298c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"/></svg>
